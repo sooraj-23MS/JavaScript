3d353d70d83e7f7973503a32fe93659066b56167
SoorajMS <125868904+sooraj-23MS@users.noreply.github.com>
2024-11-06 11:10:53 +0530

    varletconst.js

--- varletconst.js
+++ varletconst.js
@@ -1,120 +1,87 @@
-// // var
+// -----------  Var  -----------
 
-// console.log(x);      // decalre anywhere in the fn.
-// var x = 10;
+var x ;
+ x = 10;
+console.log('x = '+x);
 
-// // let 
-
-// // console.log(y);   // decalre before use.
-// // let y = 01;
-
-
-// // var
-
-// function getname(){
-//         console.log(fname);
-//         var fname="marvel";    // support multiple declaration
-//         fname="marvel";
-//         var fname;
-//         var fname;
-// }
-
-// getname();
-
-// // let 
-
-// // function getName(){
-// //     console.log(fname);       // only one declaration in a scope
-// //     let fname;
-// //     fname="marvel";
-// //     let fname;
-// // }
-
-// // getName();
-
-// // var 
-
-// var x = 10;
-// console.log(x);
-// {
-//     var x = 20;         // function scope
-//     console.log(x);
-// }
-// console.log(x);
-
-
-// // let 
-
-// let y = 10;
-// console.log(y);
-// {
-//     let y = 20;
-//     console.log(y);    // block scope
-// }
-// console.log(y);
-
-//  y = 30;
-//  console.log(y);    // can be updated not redeclare
-
-// // const
-
-// //  const z;  // must be intialized
-
-// const z = 10;
-// console.log(z);
-
-// z = 20;          // cannot update or redeclare
-// console.log(z);
-
-// var d = 10;
-// {
-//     var d=20;
-//     console.log("inside "+d);
-// }
-// console.log("outside "+d);
-
-
-// function f(){
-   
-//     var ft = 1;
-//     console.log("inside "+ft);
-// }
-// // console.log("outside "+ft);
-
-// f();
-
-
-// let x=10;
-// {
-//     {
-//         let y=20;
-//     console.log(y);
-//     }
-//     // console.log(y);
-
-// }
-// console.log(x);
-
-
-// function tt(){
-//     if(true){
-//         var x = 10;
-//         console.log(x)
-//     }
-//     console.log(x);
-// }
-// tt();
-
-const x = {
-    pro1:10,
-    pro2:20
+{
+    var y = 20;
+    console.log('y = '+y)  // not block scope. 
 }
-console.log(x)
+console.log('y = '+y);
 
-x.pro3=30;
+function xyz()
+{
+    var z = 30;         // function scope.
+    console.log('z = '+z)
+}
+xyz();
+// console.log('z = '+z); 
+//--  Uncaught ReferenceError: z is not defined.
 
-console.log(x);
- x={d1:9,
-    d2:20
- }
- console.log(x)
+var zyx = 10;
+var zyx = 20; // multiple declaration possible.
+zyx = 40;     // updation possible.
+console.log('zyx = '+zyx);
+
+// -----------  Let  -----------
+
+let u ;
+u = 10;
+console.log('\n'+'u = '+u);
+
+{
+    let v = 20;
+    console.log('v = '+v);  // block scope.
+}
+// console.log('v = '+v);  
+//--  Uncaught ReferenceError: v is not defined.
+
+function uvw(){
+    let w = 30;
+    console.log('w ='+w); 
+}
+uvw();
+// console.log('w ='+w); 
+//-- Uncaught ReferenceError: w is not defined.
+
+let wvu = 10;
+// let wvu = 20;   
+// multiple declaration not possible.
+// -- Identifier 'wvu' has already been declared. 
+wvu = 40;   // updation possible
+console.log('wvu = '+wvu);
+
+// -----------  Const  -----------
+
+// const i;
+// i = 10;                  // one of the major diff.
+// console.log('i = '+i);
+// -- Missing initializer in const declaration. 
+// A const variable must be assigned a value at the time it is declared.
+
+const i = 10;
+console.log('\n'+'i = '+i);
+
+{
+    const j = 20;
+    console.log('j = '+j);   // block scope.
+}
+// console.log('j ='+j);
+// Uncaught ReferenceError: j is not defined.
+
+function ijk(){
+    let k = 30;
+    console.log('k = '+k); 
+}
+ijk();
+// console.log('k ='+k); 
+// Uncaught ReferenceError: k is not defined.
+
+ const kji = 40;
+//  const kji = 30;
+// -- Identifier 'kji' has already been declared.
+// kji = 20;                         
+// -- Uncaught TypeError: Assignment to constant variable.
+// updation not possible.
+ console.log('kji = '+kji);
