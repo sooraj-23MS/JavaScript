c25350a986924254c4720513f1bc0d08e9415938
SoorajMS <125868904+sooraj-23MS@users.noreply.github.com>
2025-03-28 14:48:11 +0530

    New Method

--- MaxMinInArray.js
+++ MaxMinInArray.js
@@ -15,3 +15,22 @@ for (let i=0;i<minmaxarr.length;i++){
 console.log(minmaxarr);
 console.log("min = "+ minmaxarr[0])
 console.log("max = "+ minmaxarr[minmaxarr.length-1])
+
+// Other simple method.
+
+function minmax(arr){
+    let min = arr[0];
+    let max = arr[0];
+    for(let num of arr){
+        if(num < min){
+            min = num;
+        }
+        if(num > max){
+            max = num
+        }
+    }
+    console.log(arr)
+    console.log(`Min in Array ${min} .Max in Array ${max}`)
+
+}
+minmax([99, 5, 3, 100, 1])
